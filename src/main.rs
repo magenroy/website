ced8a1f3f39b8d4edd8f5564043e7812fbe8e077
Roy Magen <magenroy@gmail.com>
2024-12-25 10:47:56 -0500

    missed >

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,7 @@ fn Button(increment: i32) -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <button on:click= move |_| {set_count(count() + increment)} "Click me: " {count} </button>
+        <button on:click= move |_| {set_count(count() + increment)}> "Click me: " {count} </button>
     }
 }
 fn main() {
