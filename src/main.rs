ff4a561e7db3480e7dce0234e3bddfc084ed4f1c
Roy Magen <magenroy@gmail.com>
2025-01-26 16:10:27 -0500

    new website

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,6 @@
 use leptos::prelude::*;
 
 #[component]
-fn LeptosHellowWorld(increment: i32) -> impl IntoView {
-    let (count, set_count) = signal(0);
-
-    view! {
-    <div class="container">
-
-            <picture>
-                <source srcset="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_pref_dark_RGB.svg" media="(prefers-color-scheme: dark)" />
-                <img src="https://raw.githubusercontent.com/leptos-rs/leptos/main/docs/logos/Leptos_logo_RGB.svg" alt="Leptos Logo" height="200" width="400"/>
-            </picture>
-
-        <h1>"Welcome to Leptos"</h1>
-        <h2><i>"On Vercel"</i></h2>
-
-        <button
-            on:click= move |_| {
-                set_count(count() + increment)
-            }
-        >
-            "Click me: "
-            {count}
-        </button>
-
-
-    </div>
-    }
-}
-
-#[component]
 fn Button(increment: i32) -> impl IntoView {
     let (count, set_count) = signal(0);
     view! {
